1361187218262b736a72126da18c10bac8fb1427
Doruk <dorukbiyikli@enpos.com.tr>
2026-03-10 10:46:14 +0300

    Add Chatbase widget

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,40 @@
-// pages/_app.js
-import '../styles/globals.css'
 import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-
-      {/* Chatbase Web Widget */}
       <Script
+        id="chatbase-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="QmYkrUqr0_hp0BFPP8Wgw";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`
+          __html: `
+            (function(){
+              if(!window.chatbase||window.chatbase("getState")!=="initialized"){
+                window.chatbase=(...arguments)=>{
+                  if(!window.chatbase.q){window.chatbase.q=[]}
+                  window.chatbase.q.push(arguments)
+                };
+                window.chatbase=new Proxy(window.chatbase,{
+                  get(target,prop){
+                    if(prop==="q"){return target.q}
+                    return(...args)=>target(prop,...args)
+                  }
+                })
+              }
+              const onLoad=function(){
+                const script=document.createElement("script");
+                script.src="https://www.chatbase.co/embed.min.js";
+                script.id="QmYkrUqr0_hp0BFPP8Wgw";
+                script.domain="www.chatbase.co";
+                document.body.appendChild(script)
+              };
+              if(document.readyState==="complete"){onLoad()}
+              else{window.addEventListener("load",onLoad)}
+            })();
+          `
         }}
       />
     </>
   )
 }
-
-export default MyApp
